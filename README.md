dbf28b65ab976b6af5199c117996880dcbc4e29b
miguelrr21 <miguelangelrodriguezromero.eu@gmail.com>
2026-07-28 17:25:05 +0200

    Brute-force analysis

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Python tool to parse authentication logs and detect brute-force patterns, built 
 I created a sample auth.log file and used regex to parse each line, extracting the hour, access result (Failed/Accepted), username and IP.
 
 I store the results in two separate dictionaries (one for failed attempts, one for successful accesses), grouped by IP, so I can later cross-reference them — e.g. an IP with many failed attempts followed by a successful login is a stronger signal than failed attempts alone.
+
+## V2 
+
+Now, we check the IP and the logs attempts and its diference in time. If the attacker tried to log in 5 times in 60 seconds we receive an alert.
+
+To log the time (since it used to be stored as string) I used datetime, so I can properly check the time.
+
+After detecting a brute force attack, we compare the ip with the successful log dictionary (accesos_por_ip), if the IP is logged, a severe warning is shown since the brute force attack actually worked.
--- src/analyzer.py
+++ src/analyzer.py
@@ -1,10 +1,10 @@
 import re
+from datetime import datetime
 
 with open("../samples/auth.log", "r") as archivo:
 
     intentos_por_ip = {}
     accesos_por_ip = {}
-
     for linea in archivo:
         linea = linea.strip()
         patron = r"(\d{2}:\d{2}:\d{2}).*(Failed|Accepted).*?(?:invalid user )?(\w+) from (\d+\.\d+\.\d+\.\d+)" 
@@ -33,3 +33,17 @@ with open("../samples/auth.log", "r") as archivo:
         print(f"IP: {ip} - Intentos fallidos: {len(horas)} - Horas: {horas}")
     for ip, horas in accesos_por_ip.items():
         print(f"IP: {ip} - Accesos: {len(horas)} - Horas: {horas}")
+
+    for ip, lista_horas in intentos_por_ip.items():
+        intentos_convertidos = []
+        for horas in lista_horas:
+            nueva_hora = datetime.strptime(horas, "%H:%M:%S")
+            intentos_convertidos.append(nueva_hora)
+        segundos_diferencia = intentos_convertidos[-1] - intentos_convertidos[0]
+        segundos = segundos_diferencia.total_seconds()
+        if (segundos <= 60 and  len(intentos_convertidos) >= 5):    
+            if ip in accesos_por_ip:
+                print(f"ALERTA!!! Tras {len(intentos_convertidos)} la IP {ip} ha logrado acceder correctamente en {segundos} segundos a la hora {accesos_por_ip[ip][0]}")
+            else:
+                print(f"IP {ip} SOSPECHOSA: {len(intentos_convertidos)} intentos en {segundos} segundos")
+
